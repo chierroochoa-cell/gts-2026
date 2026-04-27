2e4a3be2c4cc453f03bb17f83ea3f42ddbc96413
chierroochoa-cell <c.hierroochoa@gmail.com>
2026-04-27 21:55:49 +0200

    Implementada API de cobertura (Back)

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("¡Hola, mundo tele     co!\n");
+    printf("¡Hola, mundo teleco!\n");
+    printf("Iniciando API de cálculo de cobertura...\n");
+    printf("Analizando línea de visión con el HAPS...\n");
     return 0;
 }
